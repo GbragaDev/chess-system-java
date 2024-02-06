da143b0c3f85575c0030a33e873f898891514411
GbragaDev <guilhermebraga1626@gmail.com>
2024-02-06 01:03:08 -0300

    Comments were added + Exception Class was started

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -1,47 +1,66 @@
+// Define o pacote ao qual a classe Board pertence.
 package boardgame;
 
+// Declaração da classe Board, que representa o tabuleiro do jogo.
 public class Board {
-
+    // Variáveis privadas para armazenar o número de linhas e colunas do tabuleiro.
     private int rows;
     private int columns;
 
+    // Matriz bidimensional para armazenar as peças do jogo.
     private Piece[][] pieces;
 
+    // Construtor da classe Board. Inicializa o tabuleiro com o número especificado de linhas e colunas.
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
     }
-
+    // Método getter para retornar o número de linhas do tabuleiro.
     public int getRows() {
         return rows;
     }
 
+    // Método setter para definir o número de linhas do tabuleiro.
     public void setRows(int rows) {
         this.rows = rows;
     }
 
+    // Método getter para retornar o número de colunas do tabuleiro.
     public int getColumns() {
         return columns;
     }
 
+    // Método setter para definir o número de colunas do tabuleiro.
     public void setColumns(int columns) {
         this.columns = columns;
     }
 
-    public Piece piece (int rows,int columns){
+    // Método para retornar uma peça do tabuleiro na posição dada pelas linhas e colunas.
+    public Piece piece(int rows, int columns) {
         return pieces[rows][columns];
     }
 
-    public Piece piece (Position position){
+    // Método sobrecarregado para retornar uma peça do tabuleiro dada uma posição.
+    public Piece piece(Position position) {
         return pieces[position.getRow()][position.getColumn()];
     }
 
-    public void placePiece(Piece piece,Position position) {
+    // Método para colocar uma peça em uma determinada posição no tabuleiro.
+    public void placePiece(Piece piece, Position position) {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
-
     }
+    // Método privado para verificar se uma posição existe no tabuleiro com base em suas coordenadas de linha e coluna.
+    private boolean positionExists( int row, int column){
+            return row >= 0 && row < rows && column >= 0 && column < columns;
+        }
+
+    // Método público para verificar se uma posição existe no tabuleiro usando um objeto Position.
+        public boolean positionExists (Position position){
+            return positionExists(position.getRow(),position.getColumn());
+        }
+
 
     }
 
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -7,25 +7,36 @@ import chess.pieces.Rook;
 
 
 public class ChessMatch {
+    // Declara uma variável privada do tipo Board para representar o tabuleiro de xadrez.
     private Board board;
 
+
+    // Construtor da classe ChessMatch. É chamado ao criar uma nova partida de xadrez.
     public ChessMatch() {
         board = new Board(8, 8);
         initialSetup();
+        // Chama o método initialSetup para configurar as peças no tabuleiro no início do jogo.
 
     }
-
+    // Método para retornar uma matriz bidimensional de peças de xadrez.
     public ChessPiece[][] getpieces() {
+        // Cria uma nova matriz de objetos ChessPiece baseada nas dimensões do tabuleiro.
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        // Itera sobre todas as linhas do tabuleiro.
         for (int i = 0; i < board.getRows(); i++) {
+            // Itera sobre todas as linhas do tabuleiro.
             for (int j = 0; j < board.getColumns(); j++) {
-                mat[i][j] = (ChessPiece) board.piece(i, j); //downcast
+                // Armazena a peça de xadrez atual na matriz após fazer o downcast para ChessPiece.
+                mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
+        // Retorna a matriz de peças de xadrez.
         return mat;
+
     }
 
     private void initialSetup(){
+        //Cria as peças no Tabuleiro com Cor e Posição
         board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
         board.placePiece(new King(board,Color.BLACK),new Position(0,4));
         board.placePiece(new King(board,Color.WHITE),new Position(7,4));
