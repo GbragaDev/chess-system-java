e834af34023b12166927045d469294a258d30f74
GbragaDev <guilhermebraga1626@gmail.com>
2024-02-16 17:04:09 -0300

    Moving pieces is added

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -1,108 +1,78 @@
 // Define o pacote ao qual a classe Board pertence.
 package boardgame;
 
-import chess.pieces.King;
+
 
 // Declaração da classe Board, que representa o tabuleiro do jogo.
 public class Board {
-    // Variáveis privadas para armazenar o número de linhas e colunas do tabuleiro.
-    private int rows;
-    private int columns;
+    private final int rows;
+    private final int columns;
+    private final Piece[][] pieces;
 
-    // Matriz bidimensional para armazenar as peças do jogo.
-    private Piece[][] pieces;
-
-    // Construtor da classe Board. Inicializa o tabuleiro com o número especificado de linhas e colunas.
     public Board(int rows, int columns) {
         if (rows < 1 || columns < 1) {
-            throw new BoardException("Error creating board : there must be at least 1 row and 1 column");
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
     }
 
-    // Método getter para retornar o número de linhas do tabuleiro.
     public int getRows() {
         return rows;
     }
 
-    /* Metodo setter retirado para que a quantidade de linhas não seja alterada
-
-    public void setRows(int rows) {
-        this.rows = rows;
-
-    }
-*/
-    // Método getter para retornar o número de colunas do tabuleiro.
     public int getColumns() {
         return columns;
     }
 
-
-    /*
-        Método setter retirado para nao alterar o número de colunas
-
-        public void setColumns(int columns) {
-        this.columns = columns;
-
-    }
-*/
-
-    // Método para retornar uma peça do tabuleiro na posição dada pelas linhas e colunas.
-    public Piece piece(int rows, int columns) {
-        // Verifica se a posição está dentro dos limites do tabuleiro.
-        if (!positionExists(rows, columns)) {
-            // Lança uma exceção se a posição não estiver no tabuleiro.
+    public Piece piece(int row, int column) {
+        if (positionExists(row, column)) {
             throw new BoardException("Position not on the board");
         }
-        return pieces[rows][columns];
+        return pieces[row][column];
     }
 
-    // Método sobrecarregado para retornar uma peça do tabuleiro dada uma posição.
     public Piece piece(Position position) {
-        // Verifica se a posição está dentro dos limites do tabuleiro.
-        if (!positionExists(position)) {
-            // Lança uma exceção se a posição não estiver no tabuleiro.
+        if (positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
-        // Retorna a peça na posição especificada.
         return pieces[position.getRow()][position.getColumn()];
-    }
-
-    // Método para colocar uma peça em uma determinada posição no tabuleiro.
-
-
-    // Método privado para verificar se uma posição existe no tabuleiro com base em suas coordenadas de linha e coluna.
-    private boolean positionExists(int row, int column) {
-        return row >= 0 && row < rows && column >= 0 && column < columns;
-    }
-
-    // Método público para verificar se uma posição existe no tabuleiro usando um objeto Position.
-    public boolean positionExists(Position position) {
-        return positionExists(position.getRow(), position.getColumn());
-    }
-
-    // Método para verificar se há uma peça em uma determinada posição do tabuleiro.
-    public boolean thereIsAPiece(Position position) {
-        // testa se a posição existe e lança a exceção antes de retornar se há uma peça na position
-        if (!positionExists(position)) {
-            throw new BoardException("Theres already a piece on position" + position);
-        }
-        return piece(position) != null;
-        // Verifica se a posição não é nula.
-
     }
 
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position)) {
-            throw new BoardException("Theres already a piece on position" + position);
-
+            throw new BoardException("There is already a piece on position " + position);
         }
-
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
 
+    private boolean positionExists(int row, int column) {
+        return row < 0 || row >= rows || column < 0 || column >= columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
+    }
 }
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,7 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.ChessException;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -18,6 +20,7 @@ public class ChessMatch {
         // Chama o método initialSetup para configurar as peças no tabuleiro no início do jogo.
 
     }
+
     // Método para retornar uma matriz bidimensional de peças de xadrez.
     public ChessPiece[][] getpieces() {
         // Cria uma nova matriz de objetos ChessPiece baseada nas dimensões do tabuleiro.
@@ -33,6 +36,27 @@ public class ChessMatch {
         // Retorna a matriz de peças de xadrez.
         return mat;
 
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturedPiece = makeMove(source,target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source ,Position target){
+        Piece p = board.removePiece(source);
+        Piece capturedPiece =board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+
+    private void validateSourcePosition(Position position){
+        if(!board.thereIsAPiece(position)){
+            throw new ChessException("Theres is no piece on source position");
+        }
     }
 
     private void placeNewPiece(char column ,int row ,ChessPiece piece){
@@ -56,7 +80,8 @@ public class ChessMatch {
         placeNewPiece('e',7, new Rook(board,Color.BLACK));
         placeNewPiece('e',8, new Rook(board,Color.BLACK));
         placeNewPiece('d',8, new King(board,Color.BLACK));
-    }
 
-}
+            }
+
+        }
 
